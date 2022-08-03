b322fa5d12ac86a5fb5e8d96de14499310edb296
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2022-08-04 08:50:36 +0900

    イメージ名の変更

--- build.bash
+++ build.bash
@@ -4,5 +4,7 @@ $* \
 --progress=plain \
 --build-arg http_proxy=$http_proxy \
 --build-arg https_proxy=$https_proxy \
--t fcwu-test \
+--build-arg HTTP_PROXY=${HTTP_PROXY} \
+--build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+-t airobotbook/ros2-desktop-ai-robot-book:ver0 \
 .
