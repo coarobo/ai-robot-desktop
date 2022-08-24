6ce1d131990c4acc2e9f6290dc8a5c2e1751d0a5
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2022-08-24 20:59:38 +0900

    ビルド時にタグ名を指定しないようにした

--- build.bash
+++ build.bash
@@ -6,5 +6,5 @@ $* \
 --build-arg https_proxy=${https_proxy} \
 --build-arg HTTP_PROXY=${HTTP_PROXY} \
 --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
--t airobotbook/ros2-desktop-ai-robot-book:rdp \
+-t airobotbook/ros2-desktop-ai-robot-book \
 .
