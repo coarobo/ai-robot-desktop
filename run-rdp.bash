02e830806af1a9ad01853aa9017c8fb8944d82dd
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2022-08-28 13:01:41 +0900

    起動シェルスクリプトを2つに分ける．

--- run-rdp.bash
+++ run-rdp.bash
@@ -5,13 +5,8 @@ docker run \
 -e HTTP_PROXY=${HTTP_PROXY} \
 -e HTTPS_PROXY=${HTTPS_PROXY} \
 -e RESOLUTION=1920x1080 \
--e PULSE_COOKIE=/tmp/pulse/cookie \
--e PULSE_SERVER=unix:/tmp/pulse/native \
--v /run/user/$(id -u)/pulse/native:/tmp/pulse/native \
--v ~/.config/pulse/cookie:/tmp/pulse/cookie:ro \
 --name ai_robot_book \
--p 15900:5900 \
--p 6080:80 \
+-p 13389:3389 \
 --shm-size=512m \
 --privileged \
 $* \
